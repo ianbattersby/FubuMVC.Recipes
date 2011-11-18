4a62f2b641d4f8f63ab214d34457893973a4a111
Francisco Ruiz A <frankra@gmail.com>
2011-11-18 12:37:22 +0000

    File demo. This project demonstrate how to stream files to browser

---
+++ src/File/DownloadStream/DownloadStream/ConfigureFubuMVC.cs
@@ -0,0 +1,32 @@
+namespace DownloadStream
+{
+    using DownloadStream.Controllers;
+    using FubuMVC.Core;
+    using FubuMVC.Core.Registration.Conventions;
+
+    public class ConfigureFubuMVC : FubuRegistry
+    {
+        public ConfigureFubuMVC()
+        {
+            // This line turns on the basic diagnostics and request tracing
+            IncludeDiagnostics(true);
+
+            ApplyConvention<DownloadFileConvention>();
+
+            // All public methods from concrete classes ending in "Controller"
+            // in this assembly are assumed to be action methods
+            Actions.IncludeClassesSuffixedWithController();
+
+            // Policies
+            Routes
+                .HomeIs<HomeController>(home => home.Index())
+                .IgnoreControllerNamespaceEntirely()
+                .IgnoreMethodSuffix("Html")
+                .RootAtAssemblyNamespace();
+
+            // Match views to action methods by matching
+            // on model type, view name, and namespace
+            Views.TryToAttachWithDefaultConventions();
+        }
+    }
+}
---
+++ src/File/DownloadStream/DownloadStream/Controllers/PdfController.cs
@@ -0,0 +1,28 @@
+﻿namespace DownloadStream.Controllers
+{
+    using System.Web;
+
+    using FubuMVC.Core.Behaviors;
+
+    public class PdfController
+    {
+        public DownloadFileModel Stream()
+        {
+            return new DownloadFileModel
+            {
+                ContentType = "application/pdf",
+                LocalFileName = HttpContext.Current.Server.MapPath("~/Assets/hola.pdf")
+            };
+        }
+
+        public DownloadFileModel Download()
+        {
+            return new DownloadFileModel
+            {
+                ContentType = "application/pdf",
+                FileNameToDisplay = "hola.pdf",
+                LocalFileName = HttpContext.Current.Server.MapPath("~/Assets/hola.pdf")
+            };
+        }
+    }
+}
